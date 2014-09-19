20494eab08f5234e0f07792a92f04e856c7c7451
mrg58 <goulet.michaelrobert@gmail.com>
2014-09-19 15:23:46 -0400

    name

---
+++ KeyboardInput/NameAgeSalary.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NameAgeSalary 
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		String name;
+		int age;
+		float salary;
+
+		System.out.println( "Hello. What is your name?" );
+		name = keyboard.next();
+	
+		System.out.println();
+		System.out.println( "Hi " + name + "! How old are you?" );
+		age = keyboard.nextInt();
+
+		System.out.println();
+		System.out.println( "So you're " + age + ", eh? That's not old at all!" );
+		System.out.println( "How much do you make, " + name + "?" );
+		salary = keyboard.nextFloat();
+
+		System.out.println(salary + "! I hope that's per hour and not per year! LOL!" );
+	}
+}
