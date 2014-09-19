dfbdf6e6c8e25e53e0d38b69fc28a67b6bb8ff31
mrg58 <goulet.michaelrobert@gmail.com>
2014-09-19 15:02:34 -0400

    update

--- KeyboardInput/AskingQuestions.java
+++ KeyboardInput/AskingQuestions.java
@@ -6,19 +6,21 @@ public class AskingQuestions
 	{
 		Scanner keyboard = new Scanner(System.in);
 		
-		int age;
-		String height;
+		int age, height, inches;
 		double weight;
 	
 		System.out.print( "How old are you? " );
 		age = keyboard.nextInt();
 		
-		System.out.print( "How tall are you? " );
-		height = keyboard.next();
+		System.out.print( "How many feet tall are you? " );
+		height = keyboard.nextInt();
+
+		System.out.print( "How many inches tall are you? " );
+		inches = keyboard.nextInt();
 
 		System.out.print( "How much do you weight? " );
 		weight = keyboard.nextDouble();
 
-		System.out.println( "So you're " + age + " old, " + height + " tall and " + weight + " heavy." );
+		System.out.println( "So you're " + age + " old, " + height + "'" + inches + "\"" +  " tall and " + weight + " heavy." );
 	}
 }
