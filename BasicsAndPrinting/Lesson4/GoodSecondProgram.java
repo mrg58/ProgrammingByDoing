57e945c27bde13acadbd010adf849849ab55949d
mrg58 <goulet.michaelrobert@gmail.com>
2014-09-19 13:03:34 -0400

    test

---
+++ BasicsAndPrinting/Lesson4/GoodSecondProgram.java
@@ -0,0 +1,15 @@
+public class GoodSecondProgram
+{
+	public static void main( String[] args )
+	{
+		// System.out.println( "Hello World!" );
+		// The slashes are comments!
+		System.out.println( "Hello Again" );
+		System.out.println( "I like typing this." );
+		System.out.println( "This is fun." );
+		System.out.println( "Yay! Printing." );
+		System.out.println( "I'd much rather you 'not'." );
+		System.out.println( "I \"said\" do not touch t his." );
+		System.out.println( "Additional line of output." );
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Programming By Doing
+
+In my quest to learn how to program using Java, I am working through the exercises found on the *Programming By Doing* website. 
